e9232e93dda6b377e8b656359d706b929e76855d
cako1911 <ata.cakiroglu@hotmail.com>
2025-03-11 11:28:45 +0100

    basic_data_structures.py
    ata_cakiroglu

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+my_age = 22 #int
+my_age_float = 22.5  #float / because my birthday is 5th mounth 
+my_name = “ata” #str
+my_fav_colors = [“blue” , “green” , “yellow”]  #list
+set = {“a” , “b”}  #set
+my_age_23 = False  #bool
 
-p1 = Person("Ata", 22)
-
-print(p1.name)
-print(p1.age)
+git add .
+git commit -m 'Assign data structures to variables'
+git push origin main
