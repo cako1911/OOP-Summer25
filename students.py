55a477843ca9cf76e5a2aba57e2c9e6ec1f7c3fb
cako1911 <ata.cakiroglu@hotmail.com>
2025-03-13 10:39:31 +0100

    students.py

---
+++ students.py
@@ -0,0 +1,23 @@
+student1 = {
+    'first_name': 'Ata' ,
+    'last_name' : 'Cakirglu'
+}
+
+student2 = {
+    'first_name': 'Tuna' ,
+    'last_name' : 'Kasap'
+}
+
+student3 = {
+    'first_name': 'Kaan' ,
+    'last_name' : 'Turan'
+}
+
+print(student1.first_name)
+print(student1.last_name)
+
+print(student2.first_name)
+print(student2.last_name)
+
+print(student3.first_name)
+print(student3.last_name)
