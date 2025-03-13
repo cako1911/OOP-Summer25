3988e91829b6fc3e84be4d13163cdf609659e361
cako1911 <ata.cakiroglu@hotmail.com>
2025-03-13 10:29:04 +0100

    student_details_structure.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-my_age = 22 #int
-my_age_float = 22.5  #float / because my birthday is 5th mounth 
-my_name = “ata” #str
-my_fav_colors = [“blue” , “green” , “yellow”]  #list
-set = {“a” , “b”}  #set
-my_age_23 = False  #bool
+student1 = {
+    'first_name': 'Ata' ,
+    'last_name' : 'Cakirglu'
+}
+
+print(student1.first_name)
+print(student1.last_name)
